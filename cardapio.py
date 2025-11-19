591be01719e43e188f23de59eb3bb6f02f694e18
devjuniormauricio <devjuniormauricio@gmail.com>
2025-11-19 00:36:07 +0000

    Buscando pelo id

--- cardapio.py
+++ cardapio.py
@@ -9,3 +9,11 @@ def buscar_cardapio():
         }
     ]
     return itens
+
+def buscar_por_id(item_id):
+    return{
+        "id": 1,
+        "nome": "Chocomoça",
+        "descricao": "Bolo de chocolate com ninho",
+        "preco": 49.99
+    }
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 
 from flask import Flask
-from cardapio import buscar_cardapio
+from cardapio import buscar_cardapio, buscar_por_id
 
 app = Flask(__name__)
 
@@ -15,6 +15,9 @@ def hello_world():
 def cardapio():
   return buscar_cardapio()
 
+@app.route("/cardapio/<int:item_id>")
+def cardapio_por_id(item_id):
+  return buscar_por_id(item_id)
 
 if __name__ == "__main__":
   app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 3000)))
